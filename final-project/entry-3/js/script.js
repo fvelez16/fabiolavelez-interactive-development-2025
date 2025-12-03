ec060d1fb25cb2809f66b2786c3e6ab24bb8c8f3
fvelez16 <fvelez00@uafs.edu>
2025-12-03 12:37:26 -0600

    u

--- final-project/entry-3/js/script.js
+++ final-project/entry-3/js/script.js
@@ -1,10 +1,3 @@
-var messageArray=["The Killing Moon by Echo and the Bunnymen"];
-var textPosition = 0;
-var speed= 55;
-typewriter = () => {
-    document.querySelector("#message").
-    innerHTML = messageArray [0].substring(0,textPosition) + "<span>\u25ae</span>";
-    if(textPosition++ != messageArray [0].length)
-        setTimeout (typewriter, speed);
-}
-window.addEventListener("load" , typewriter); 
+jQuery(document).ready(function(){
+    jQuery("#glowing-text").glow();
+})
