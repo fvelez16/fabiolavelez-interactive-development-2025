6961b36ef6368d26c44bd016cd1eb8c4614becf4
fvelez16 <fvelez00@uafs.edu>
2025-12-10 08:05:30 -0600

    u

--- final-project/entry-1/js/script.js
+++ final-project/entry-1/js/script.js
@@ -1,10 +1,3 @@
-var messageArray=["Somebody Else by the 1975"];
-var textPosition = 0;
-var speed= 25;
-typewriter = () => {
-    document.querySelector("#message").
-    innerHTML = messageArray [0].substring(0,textPosition) + "<span>\u25ae</span>";
-    if(textPosition++ != messageArray [0].length)
-        setTimeout (typewriter, speed);
-}
-window.addEventListener("load" , typewriter); 
+$("#message").on("click", function(){
+    $("#message").toggleClass("title-change")
+})
